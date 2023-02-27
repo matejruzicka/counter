92c78bbdfa82e9dec775499f3db2e6bce03543da
Matěj Růžička <matejruzicka@Matejs-MacBook-Air.local>
2023-02-27 19:10:25 +0100

    feat(logout): logout alert made dynamic

--- counter/urls.py
+++ counter/urls.py
@@ -1,18 +1,3 @@
-"""counter URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import TemplateView
@@ -28,5 +13,5 @@ urlpatterns = [
     path('delete/<int:counter_id>', views.delete, name='delete'),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     path('login/', authentication_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', authentication_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('logout/', authentication_views.LogoutView.as_view(), name='logout'),
 ]
